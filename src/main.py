ae0b42775fd9fa971f2d16ce15758375405d0091
Robertomirax <52511086+Robertomirax@users.noreply.github.com>
2022-01-11 15:50:18 -0300

    ver

--- src/main.py
+++ src/main.py
@@ -1,53 +1,17 @@
-import utime
-from machine import Pin
-import micropython
-micropython.alloc_emergency_exception_buf(100)
+from lib.rebotes import Rebotes
 
-class Rebotes():
-    """Elimina los rebotes al pulsar una tecla
-    (pin=pin gpio, pull=PULL_UP o PULL_DOWN,
-    rutina=mombre de la rutina que atiende la interrupcion,
-    flanco=IRQ_FALLING o IRQ_RISING, retardo=tiempo de retardo en ms)
-    """
-    def __init__(self, pin=None, pull='PULL_UP', rutina=None, flanco='IRQ_FALLING', retardo=200):
-        self.pin = pin
-        self.pull = pull
-        self.rutina = rutina
-        self.flanco = flanco
-        self.retardo = retardo
-        self.veces = 0
-        self.momento = 0.00
-        self.nivel = 0
-        self.tiempo = 0.00
-        self.pasaron = 0.00
+pres1 = 0
+pres2 = 0
+def bot1():
+    global pres1
+    print('presionaste el boton 1 ', pres1, ' veces')
+    pres1 +=1
 
-        if pull == 'PULL_UP':
-            self.sw1 = Pin(self.pin, Pin.IN, Pin.PULL_UP)
-        else:
-            self.sw1 = Pin(self.pin, Pin.IN, Pin.PULL_DOWN)
-        
-        if self.flanco == 'IRQ_FALLING':
-            self.sw1.irq(handler = self.inte, trigger=Pin.IRQ_FALLING)
-            self.flanco = 0
-        else:
-            self.sw1.irq(handler = self.inte, trigger=Pin.IRQ_RISING)
-            self.flanco = 1
+def bot2():
+    global pres2
+    print('presionaste el boton 2 ', pres2, ' veces')
+    pres2 +=1
 
-    def inte(self,p):
-        self.nivel = self.sw1.value()
-        if self.nivel == self.flanco:
-            self.tiempo = utime.ticks_ms()
-            self.pasaron = self.tiempo - self.momento
-            self.momento = self.tiempo
-            if self.pasaron > 200:
-                self.veces +=1
-                print('se presiono el boton ', self.veces, ' veces         nivel ', self.nivel, '          ms ', self.pasaron)
-                self.rutina()
-    
-    
-        
+boton1 = Rebotes(12,rutina=bot1)
+boton2 = Rebotes(14,rutina=bot2)
 
-def boton1():
-    print('se llamó a boton1')
-
-boto = Rebotes(12, rutina=boton1, flanco='IRQ_FALLING')
